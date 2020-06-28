535bc1e9aeabed27dac9556dbce8047aab659c92
Adam Jaamour <adam@jamour.com>
2020-06-28 17:37:49 +0200

    Write function to show raw PIL image

--- src/utils.py
+++ src/utils.py
@@ -6,3 +6,12 @@ def print_runtime(text: str, runtime: float) -> None:
     :return: None.
     """
     print("\n--- {} runtime: {} seconds ---".format(text, runtime))
+
+
+def show_raw_image(img) -> None:
+    """
+    Displays a PIL image.
+    :param img: the image in PIL format (before being converted to an array).
+    :return: None.
+    """
+    img.show()
