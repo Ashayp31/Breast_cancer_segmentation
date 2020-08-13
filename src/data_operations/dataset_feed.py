7141a12ec3a56c24ef5a694833a663254ce4aed8
Ashayp31 <62710884+Ashayp31@users.noreply.github.com>
2020-08-13 15:53:58 +0100

    Update dataset_feed.py

--- src/data_operations/dataset_feed.py
+++ src/data_operations/dataset_feed.py
@@ -289,12 +289,12 @@ def parse_function_crops(filename_original,filename_mask):
     image_mask = (image_mask - current_min_mask) / (current_max_mask - current_min_mask)
     image_mask = tf.reshape(image_mask, [1, config.VGG_IMG_SIZE_LARGE['HEIGHT'], config.VGG_IMG_SIZE_LARGE['WIDTH'],1])
     
-    
+    # Get bounding box of segmentation mask
     [minr, minc, maxr, maxc] = tf_function(image_mask)
     bbox_height = maxr - minr
     bbox_width = maxc - minc
     
-    # from bounding box size to wanted image size get required expansion of bounding box needed
+    # From the size of the bounding box and the size of the required crop get the difference in order to give size of expansion of bounding box required
     if bbox_height < config.VGG_IMG_SIZE['HEIGHT']:
         height_to_add = config.VGG_IMG_SIZE['HEIGHT']-bbox_height
     else:
@@ -304,7 +304,8 @@ def parse_function_crops(filename_original,filename_mask):
         width_to_add = config.VGG_IMG_SIZE['WIDTH']-bbox_width
     else:
         width_to_add = 0
-        
+    
+    # Crop image to calculated crop dimensions and location
     if (height_to_add == 0) or (width_to_add == 0):
         image = tf.image.crop_to_bounding_box(image, minr, minc, maxr-minr, maxc-minc)
         image_mask = tf.image.crop_to_bounding_box(image_mask, minr, minc, maxr-minr, maxc-minc) 
@@ -324,7 +325,7 @@ def parse_function_crops(filename_original,filename_mask):
         minc = minc - width_to_add//2
         maxc = maxc + width_extra + width_to_add//2
 
-        # If cropping and resizing takes us outside image bounds we move the crop to the sides
+        # If cropping and resizing takes us outside image bounds we move the crop
         if minr < 0:
             maxr = maxr + abs(minr)
             minr = 0
@@ -355,6 +356,7 @@ def parse_function_crops(filename_original,filename_mask):
 
 @tf.function()
 def tf_function(mask_image):
+    # Function to get bounding box of image segmentation masks#
     y = tf.numpy_function(get_bbox_of_mask, [mask_image], [tf.int32, tf.int32, tf.int32, tf.int32])
     return y 
 
@@ -372,183 +374,3 @@ def get_bbox_of_mask(mask):
 
 
 
-
-
-# def bbox_dataset(lines):
-#     """
-#     Genereate a tensorflow dataset for feeding in the data
-#     :param x: X inputs - paths to images
-#     :param y: y values - labels for images
-#     :return: the dataset
-#     """
-#     dataset = tf.data.Dataset.from_tensor_slices(lines)
-#     dataset = dataset.map(parse_function_bbox, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-#     # Dataset to cache data and repeat until all samples have been run once in each epoch
-#     dataset = dataset.cache().repeat(1)
-#     dataset = dataset.batch(config.BATCH_SIZE)
-#     dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
-
-#     return dataset
-
-
-# def parse_function_bbox(line):
-#     anchors_path = 'keras_yolo3/model_data/yolo_anchors.txt'
-#     anchors = get_anchors(anchors_path)
-#     image = get_image(line)
-#     box_data = tf_function_1(line)
-#     y_true = tf_function_2(box_data, anchors)
-#     return image, y_true
-
-
-# def get_image(annotation_line):
-#     """
-#     Original code taken from yolo3 implementation code at : https://github.com/qqwweee/keras-yolo3
-#     Adapted for CBIS-DDSM dataset input and preprocessing
-#     """
-#     line = tf.strings.split(annotation_line)
-
-#     image_bytes = tf.io.read_file(line[0])
-#     image = tfio.image.decode_dicom_image(image_bytes, color_dim = True,  dtype=tf.uint16)
-#     image = tf.image.resize_with_pad(image, config.VGG_IMG_SIZE['HEIGHT'], config.VGG_IMG_SIZE['WIDTH'])
-#     if config.prep == "Y":
-#         image /= 256
-#         image = gaussian_blur(image)
-#         image = tf_equalize_histogram(image)
-#     current_min = tf.reduce_min(image)
-#     current_max = tf.reduce_max(image)
-#     image = (image - current_min) / (current_max - current_min)
-#     image_data = tf.reshape(image, [config.VGG_IMG_SIZE['HEIGHT'], config.VGG_IMG_SIZE['WIDTH'],1])
-
-#     return image_data
-
-
-
-# @tf.function(input_signature=[tf.TensorSpec(None, tf.string)])
-# def tf_function_1(input):
-#     y = tf.numpy_function(my_numpy_func_1, [input], tf.int32)
-#     return y 
-
-
-# def my_numpy_func_1(annotation_line):
-#     """
-#     Original code taken from yolo3 implementation code at : https://github.com/qqwweee/keras-yolo3
-#     Adapted for CBIS-DDSM dataset input and preprocessing
-#     """
-#     print("WE HERE FOR THIS ONE")
-#     line = annotation_line.split()
-
-#     iw, ih = config.VGG_IMG_SIZE["HEIGHT"], config.VGG_IMG_SIZE["WIDTH"] 
-#     h, w = config.VGG_IMG_SIZE["HEIGHT"], config.VGG_IMG_SIZE["WIDTH"]
-    
-#     box = np.array([np.array(list(map(int,box.split(',')))) for box in line[1:]])
-
-#     # resize image
-#     scale = min(w/iw, h/ih)
-#     nw = int(iw*scale)
-#     nh = int(ih*scale)
-#     dx = (w-nw)//2
-#     dy = (h-nh)//2
-
-#     # correct boxes
-#     box_data = np.zeros((max_boxes,5))
-#     if len(box)>0:
-#         np.random.shuffle(box)
-#         if len(box)>max_boxes: box = box[:max_boxes]
-#         box[:, [0,2]] = box[:, [0,2]]*scale + dx
-#         box[:, [1,3]] = box[:, [1,3]]*scale + dy
-#         box_data[:len(box)] = box
-
-#     return  box_data
-
-
-
-# @tf.function()
-# def tf_function_2(boxes, anchors):
-#     y = tf.numpy_function(preprocess_true_boxes, [boxes,anchors], tf.int32)
-#     return y 
-
-
-# def preprocess_true_boxes(true_boxes, anchors):
-
-#     num_classes=1,
-#     input_shape=(config.VGG_IMG_SIZE["HEIGHT"], config.VGG_IMG_SIZE["WIDTH"])
-#     '''
-#     Original code taken from yolo3 implementation code at : https://github.com/qqwweee/keras-yolo3
-#     Adapted for CBIS-DDSM dataset input and preprocessing
-#     Preprocess true boxes to training input format
-
-#     Parameters
-#     ----------
-#     true_boxes: array, shape=(m, T, 5)
-#         Absolute x_min, y_min, x_max, y_max, class_id relative to input_shape.
-#     input_shape: array-like, hw, multiples of 32
-#     anchors: array, shape=(N, 2), wh
-#     num_classes: integer
-
-#     Returns
-#     -------
-#     y_true: list of array, shape like yolo_outputs, xywh are reletive value
-
-#     '''
-#     assert (true_boxes[..., 4]<num_classes).all(), 'class id must be less than num_classes'
-#     num_layers = len(anchors)//3 # default setting
-#     anchor_mask = [[6,7,8], [3,4,5], [0,1,2]] if num_layers==3 else [[3,4,5], [1,2,3]]
-
-#     true_boxes = np.array(true_boxes)
-#     input_shape = np.array(input_shape)
-#     boxes_xy = (true_boxes[..., 0:2] + true_boxes[..., 2:4]) // 2
-#     boxes_wh = true_boxes[..., 2:4] - true_boxes[..., 0:2]
-#     true_boxes[..., 0:2] = boxes_xy/input_shape[::-1]
-#     true_boxes[..., 2:4] = boxes_wh/input_shape[::-1]
-
-#     m = true_boxes.shape[0]
-#     grid_shapes = [input_shape//{0:32, 1:16, 2:8}[l] for l in range(num_layers)]
-#     y_true = [np.zeros((m,grid_shapes[l][0],grid_shapes[l][1],len(anchor_mask[l]),5+num_classes),
-#         dtype='float32') for l in range(num_layers)]
-
-#     # Expand dim to apply broadcasting.
-#     anchors = np.expand_dims(anchors, 0)
-#     anchor_maxes = anchors / 2.
-#     anchor_mins = -anchor_maxes
-#     valid_mask = boxes_wh[..., 0]>0
-
-#     for b in range(m):
-#         # Discard zero rows.
-#         wh = boxes_wh[b, valid_mask[b]]
-#         if len(wh)==0: continue
-#         # Expand dim to apply broadcasting.
-#         wh = np.expand_dims(wh, -2)
-#         box_maxes = wh / 2.
-#         box_mins = -box_maxes
-
-#         intersect_mins = np.maximum(box_mins, anchor_mins)
-#         intersect_maxes = np.minimum(box_maxes, anchor_maxes)
-#         intersect_wh = np.maximum(intersect_maxes - intersect_mins, 0.)
-#         intersect_area = intersect_wh[..., 0] * intersect_wh[..., 1]
-#         box_area = wh[..., 0] * wh[..., 1]
-#         anchor_area = anchors[..., 0] * anchors[..., 1]
-#         iou = intersect_area / (box_area + anchor_area - intersect_area)
-
-#         # Find best anchor for each true box
-#         best_anchor = np.argmax(iou, axis=-1)
-
-#         for t, n in enumerate(best_anchor):
-#             for l in range(num_layers):
-#                 if n in anchor_mask[l]:
-#                     i = np.floor(true_boxes[b,t,0]*grid_shapes[l][1]).astype('int32')
-#                     j = np.floor(true_boxes[b,t,1]*grid_shapes[l][0]).astype('int32')
-#                     k = anchor_mask[l].index(n)
-#                     c = true_boxes[b,t, 4].astype('int32')
-#                     y_true[l][b, j, i, k, 0:4] = true_boxes[b,t, 0:4]
-#                     y_true[l][b, j, i, k, 4] = 1
-#                     y_true[l][b, j, i, k, 5+c] = 1
-#     print(y_true)
-#     return y_true              
-
-
-# def get_anchors(anchors_path):
-#     '''loads the anchors from a file'''
-#     with open(anchors_path) as f:
-#         anchors = f.readline()
-#     anchors = [float(x) for x in anchors.split(',')]
-#     return np.array(anchors).reshape(-1, 2)
